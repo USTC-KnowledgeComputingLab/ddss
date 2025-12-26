b9b65320c5ad27ad65614bb98d78e956d5f55edc
Hao Zhang <hzhangxyz@outlook.com>
2025-12-26 20:00:03 +0800

    build: add @rollup/plugin-terser for minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
 
 export default {
     input: "ddss/main.ts",
@@ -7,7 +8,7 @@ export default {
         format: "es",
         banner: "#!/usr/bin/env node",
     },
-    plugins: [typescript()],
+    plugins: [typescript(), terser()],
     external: [
         "atsds",
         "atsds-bnf",
